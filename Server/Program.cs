3c6372cc36d1ceebac13b0b0f22e5b5a33c65fa3
stalengd <p.zamorskiy@yandex.ru>
2024-05-28 15:11:57 +0300

    Changed server to dedirect to https only in production environment

--- Server/Program.cs
+++ Server/Program.cs
@@ -33,8 +33,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
