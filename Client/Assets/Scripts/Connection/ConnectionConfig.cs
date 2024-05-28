1b0639af10870f47129edf6a62d6245d45dfe4f1
stalengd <p.zamorskiy@yandex.ru>
2024-05-28 13:58:36 +0300

    Removed irrelevant fields in ConnectionConfig

--- Client/Assets/Scripts/Connection/ConnectionConfig.cs
+++ Client/Assets/Scripts/Connection/ConnectionConfig.cs
@@ -7,11 +7,5 @@ namespace KnowledgeConquest.Client.Connection
     {
         public string BaseUrl => _baseUrl;
         [SerializeField] private string _baseUrl = "http://localhost/";
-
-        public string Username => _username;
-        [SerializeField] private string _username = "user";
-
-        public string Password => _password;
-        [SerializeField] private string _password = "123";
     }
 }
